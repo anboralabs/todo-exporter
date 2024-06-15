2fe2f4faa185a647c8a86e443bb3ca40a3dcbe1a
Darwin Algarin <dalgarins@live.com>
2024-06-15 15:43:22 -0500

    add notification after export files.

---
+++ src/main/kotlin/co/anbora/labs/todo/icons/TodoExporterIcons.kt
@@ -0,0 +1,11 @@
+package co.anbora.labs.todo.icons
+
+import com.intellij.openapi.util.IconLoader
+
+object TodoExporterIcons {
+    @JvmField
+    val TODO_32 = IconLoader.getIcon(
+        "/images/todo_32.svg",
+        javaClass
+    )
+}
